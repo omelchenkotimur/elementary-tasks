6943a7720b0248e124ea1523c3b35bf6345d1a6c
tamer <omelchenkotamer@gmail.com>
2020-05-19 12:36:54 +0300

    FastExponention tests added

---
+++ src/test/service/FastExponentiationServiceTest.java
@@ -0,0 +1,31 @@
+package test.service;
+
+import com.softserve.service.FastExponentiationService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+class FastExponentiationServiceTest {
+
+    private static FastExponentiationService fastExponentiationService;
+
+    @BeforeAll
+    static void initialization() {
+        fastExponentiationService = new FastExponentiationService();
+        System.out.println("FastExponentiationService test start");
+    }
+
+    @Test
+    void testFastExponentiationService(){
+        long expectedResult = 8;
+        long actualResult = fastExponentiationService.binaryPower(2,3);
+        assertEquals("Fail to raise a number to a power!", expectedResult, actualResult);
+    }
+
+    @AfterAll
+    static void onComplete() {
+        System.out.println("Testing successfully completed");
+    }
+}
